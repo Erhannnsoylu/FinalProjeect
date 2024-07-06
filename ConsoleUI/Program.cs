554c42b68a05835691fa4429bc5f38626c68dafa
Erhannnsoylu <soyluerhannn@gmail.com>
2024-07-06 13:35:50 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager = new ProductManager(new InmemoryProductDal());
+            
+            foreach (var product in produtManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+         
+        
+        }
+    }
+}
